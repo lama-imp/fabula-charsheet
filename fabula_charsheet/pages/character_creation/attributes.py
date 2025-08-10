c7bfd60cd1f3217eafe156ba11caaeb07a4bb38c
Marina Lakeeva <lakeyeva.marina@gmail.com>
2025-08-10 15:40:42 +0200

    Updated attributes page for localization.

--- fabula_charsheet/pages/character_creation/attributes.py
+++ fabula_charsheet/pages/character_creation/attributes.py
@@ -1,51 +1,38 @@
 import streamlit as st
 
-from data.models import Dexterity, Might, Insight, Willpower
+from data.models import Dexterity, Might, Insight, Willpower, LocNamespace
 from .creation_state import CreationState
 from .utils import set_creation_state
 from pages.controller import CharacterController
 
 
-attributes_message = """Each of a Player Character’s main Attributes (**Dexterity**, **Insight**, **Might**, and **Willpower**) is represented by a die size, from a minimum of **d6** to a maximum of **d12**.
-Bigger die sizes indicate a more trained Attribute or a stronger natural talent.
-
-- **Dexterity** measures precision, coordination, finesse and reflexes.
-- **Insight** represents observation, understanding and reasoning.
-- **Might** is a measure of strength, resilience and physical fortitude.
-- **Willpower** represents determination, charisma and the ability to influence others.
-
-Choose one of the following profiles for your hero, then distribute the corresponding die sizes among their four Attributes.
-- **Jack of All Trades**: d8, d8, d8, d8 
-- **Average**: d10, d8, d8, d6 
-- **Specialized**: d10, d10, d6, d6
-"""
-
 def build(controller: CharacterController):
-    st.title("Character attributes")
-    st.markdown(attributes_message)
+    loc: LocNamespace = st.session_state.localizator.get(st.session_state.language)
+    st.title(loc.page_attributes_title)
+    st.markdown(loc.page_attributes_title)
     dexterity = st.select_slider(
-        "Dexterity",
+        loc.attr_dexterity,
         options=[6, 8, 10, 12],
         value=8,
     )
     might = st.select_slider(
-        "Might",
+        loc.attr_might,
         options=[6, 8, 10, 12],
         value=8
     )
     insight = st.select_slider(
-        "Insight",
+        loc.attr_insight,
         options=[6, 8, 10, 12],
         value=8
     )
     willpower = st.select_slider(
-        "Willpower",
+        loc.attr_willpower,
         options=[6, 8, 10, 12],
         value=8
     )
     not_ready_for_the_next_step = (sum([dexterity, might, insight, willpower]) != 32)
     if not_ready_for_the_next_step:
-        st.error("Sum of your attributes should be equal to 32.", icon="🎲")
+        st.error(loc.page_attributes_sum_error, icon="🎲")
     try:
         controller.character.dexterity = Dexterity(base=dexterity, current=dexterity)
         controller.character.might = Might(base=might, current=might)
@@ -54,5 +41,5 @@ def build(controller: CharacterController):
     except Exception as e:
         st.error(e, icon="🚨")
 
-    if st.button("Next", disabled=not_ready_for_the_next_step):
+    if st.button(loc.page_next_button, disabled=not_ready_for_the_next_step):
         set_creation_state(CreationState.equipment)
