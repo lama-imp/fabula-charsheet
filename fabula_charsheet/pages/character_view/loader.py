4598daf39d1b5da5ada68d2b3e7894530f088b60
Marina Lakeeva <lakeyeva.marina@gmail.com>
2025-08-11 15:23:41 +0200

    Updated loader page for localization.

--- fabula_charsheet/pages/character_view/loader.py
+++ fabula_charsheet/pages/character_view/loader.py
@@ -4,44 +4,52 @@ import streamlit as st
 
 import config
 from data import saved_characters as s
-from data.models import Character
+from data.models import Character, LocNamespace
 from pages.controller import CharacterController, StateController
 from pages.character_view.utils import set_view_state, get_avatar_path
 from pages.character_view.view_state import ViewState
 
-st.set_page_config(layout="centered")
 
-
-@st.dialog(title="Delete a character")
-def delete_character(character: Character):
-    st.warning("Are you sure you want to completely delete this character?", icon="❓")
+def delete_character(character: Character, loc: LocNamespace):
+    st.warning(loc.page_delete_character_warning, icon="❓")
     c1, c2 = st.columns([0.2, 0.8])
     with c1:
-        if st.button("No"):
+        if st.button(loc.no_button):
             st.rerun()
     with c2:
-        if st.button(f"Yes, delete {character.name.title()}", icon="💀"):
+        if st.button(
+                    loc.page_delete_character_yes_button.format(name=character.name.title()),
+                    icon="💀"
+                ):
             s.SAVED_CHARS.char_list.remove(character)
-            char_path = Path(config.SAVED_CHARS_DIRECTORY, f"{character.id}.yaml")
+            char_path = Path(config.SAVED_CHARS_DIRECTORY, f"{character.name}.{character.id}.character.yaml")
             try:
                 char_path.unlink()
             except FileNotFoundError:
-                st.error("Character file does not exist.", icon="📜")
+                st.error(loc.page_delete_character_file_missing, icon="📜")
             except PermissionError:
-                st.error("You do not have permission to delete this character file.", icon="🔒")
+                st.error(loc.page_delete_character_file_permission, icon="🔒")
             avatar_path = get_avatar_path(character.id)
             if avatar_path:
                 try:
                     avatar_path.unlink()
                     st.rerun()
                 except PermissionError:
-                    st.error("You do not have permission to delete this character avatar.", icon="🔒")
+                    st.error(loc.page_delete_character_avatar_permission, icon="🔒")
             else:
                 st.rerun()
 
 
 def build(controller: CharacterController):
-    st.title("Load a character")
+    loc: LocNamespace = st.session_state.localizator.get(st.session_state.language)
+
+    @st.dialog(title=loc.page_delete_character_title)
+    def delete_character_dialog(character: Character, loc: LocNamespace):
+        delete_character(character, loc)
+
+    st.set_page_config(layout="centered")
+    st.title(loc.page_load_character_title)
+
     if s.SAVED_CHARS.char_list:
         for char in s.SAVED_CHARS.char_list:
             col1, col2, col3 = st.columns(3)
@@ -52,11 +60,11 @@ def build(controller: CharacterController):
                 else:
                     st.image(config.default_avatar_path, width=150)
             with col2:
-                st.write(f"{char.name}, level {char.level}")
+                st.write(loc.page_load_character_info.format(name=char.name, level=char.level))
             with col3:
                 load_col, delete_col = st.columns(2)
                 with load_col:
-                    if st.button("Load", key=f"{char.id}-loader"):
+                    if st.button(loc.page_load_character_load_button, key=f"{char.id}-loader"):
                         controller.character = char
                         try:
                             st.session_state.state_controller = StateController(char.id)
@@ -65,9 +73,9 @@ def build(controller: CharacterController):
                             st.toast(e)
                         set_view_state(ViewState.view)
                 with delete_col:
-                    if st.button("Delete", key=f"{char.id}-delete"):
-                        delete_character(char)
+                    if st.button(loc.page_load_character_delete_button, key=f"{char.id}-delete"):
+                        delete_character_dialog(char, loc)
 
             st.divider()
     else:
-        st.info("No saved characters. Start with creating a character.", icon="👻")
+        st.info(loc.page_load_character_no_saved, icon="👻")
